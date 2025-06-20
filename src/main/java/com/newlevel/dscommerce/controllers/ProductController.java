8017a89130878e32d83d2024ab39c553447a6687
Marco Sallas Tamassia <marco.a.s.tamassia@gmail.com>
2025-06-20 09:03:23 -0300

    Add GET product paged

--- src/main/java/com/newlevel/dscommerce/controllers/ProductController.java
+++ src/main/java/com/newlevel/dscommerce/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.newlevel.dscommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,19 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	
+	@GetMapping
+	public Page<ProductDTO>findAll(Pageable pageable){
+		return productService.findAll(pageable);
+	}
+	
 	@GetMapping("/{id}")
 	public ProductDTO teste(@PathVariable Long id) {
 		ProductDTO result =  productService.findById(id);
 		return result;
 	}
+	
+
 	
 	
 
